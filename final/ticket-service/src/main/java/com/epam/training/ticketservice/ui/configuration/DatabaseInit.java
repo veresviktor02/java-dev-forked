47d80a7aa03a2a5b0e62761d4432f261c7fa374a
Veres Viktor Árpád <veresviktor02@gmail.com>
2023-11-23 18:21:13 +0100

    feature: kezdő admin felhasználó admin-admin credentials-szel

---
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/ui/configuration/DatabaseInit.java
@@ -0,0 +1,21 @@
+package com.epam.training.ticketservice.ui.configuration;
+
+import com.epam.training.ticketservice.core.model.User;
+import com.epam.training.ticketservice.core.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class DatabaseInit {
+
+    private final UserRepository userRepository;
+
+    @PostConstruct
+    public void init() {
+        User admin = new User("admin", "admin", User.Role.ADMIN);
+        userRepository.save(admin);
+    }
+}
